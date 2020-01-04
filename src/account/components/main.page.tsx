5d391698a35eac5c4c7cf7a59c41177f8270b5e7
g4rcez <allan.f.garcez@gmail.com>
2020-01-04 03:10:10 -0300

    Add itens flux - Complete

--- src/account/components/main.page.tsx
+++ src/account/components/main.page.tsx
@@ -1,100 +1,108 @@
-import {
-  Form,
-  Icon,
-  Input,
-  Item as FormItem,
-  Label,
-  Text,
-  Title,
-  View
-} from "native-base";
-import React, { useRef } from "react";
-import { StyleSheet, TextInput } from "react-native";
+import { Text, Title, View } from "native-base";
+import React from "react";
+import { StyleSheet } from "react-native";
 import Collapse from "react-native-collapsible";
 import useReducer from "../../hooks/useReducer";
 import Button from "../../lego-native/button";
+import { formatBrlToFloat } from "../../lego-native/currency-input";
+import { textComprehension } from "../../product/icon-map";
 import colors from "../../styles/colors";
+import ConsumedItem from "../consumed-item";
 import Person from "../person";
 import PersonItem from "../person-item";
+import ListRegisterItems from "./list-register-items";
+import NewItemForm from "./new-item.form";
 
 const styles = StyleSheet.create({
   title: { color: colors.textOnLight, fontSize: 30 },
+  eyeIcon: { fontSize: 15 },
   newPerson: {
     paddingVertical: 5,
     paddingTop: 20,
     justifyContent: "center",
     width: "100%"
   },
-  textBold: { fontWeight: "bold" },
   itensView: { marginVertical: 15 },
-  personAccountTitle: {
-    fontSize: 18,
-    marginTop: 20,
-    fontWeight: "bold",
-    marginBottom: 15
+  main: { justifyContent: "center", width: "100%" },
+  container: {
+    marginVertical: 5,
+    flex: 1,
+    flexDirection: "column"
   },
-  main: { width: "100%" },
-  itemRegister: {
-    marginVertical: 10
+  fluxButtons: {
+    marginVertical: 5
   }
 });
 
 const initial = {
-  collapsedNewPerson: true as boolean,
-  persons: [] as Person[],
-  item: new PersonItem(),
-  person: new Person()
+  collapseItem: true as boolean,
+  collapsePerson: true as boolean,
+  items: [] as ConsumedItem[],
+  personItem: new PersonItem(),
+  persons: [] as Person[]
 };
 
 const Account = () => {
-  const inputRef = useRef({});
-  const { state, ...reducer } = useReducer(initial, {
-    onChangeView: () => ({
-      collapsedNewPerson: !state.collapsedNewPerson
+  const [state, dispatch] = useReducer(initial, {
+    onToggleItem: currState => ({
+      ...currState,
+      collapseItem: !currState.collapseItem
     }),
-    onChangePerson: (field: keyof Person, value: unknown) => ({
-      person: { ...state.person, [field]: value }
+    onTogglePerson: currState => ({
+      ...currState,
+      collapsePerson: !currState.collapsePerson
+    }),
+    addItem: (currState, action) => ({
+      ...currState,
+      items: [...currState.items, action.item]
+    }),
+    removeItem: (currState, action) => ({
+      ...currState,
+      items: currState.items.filter(x => x.id !== action.id)
     })
   });
 
-  console.log("INPUT REF", inputRef.current);
-  const changeName = (e: string) => reducer.onChangePerson("name", e);
+  const toggleNewItem = () => dispatch({ type: "onToggleItem" });
+
+  const toggleNewPerson = () => dispatch({ type: "onTogglePerson" });
+
+  const remove = (id: string) => () => dispatch({ type: "removeItem", id });
+
+  const submit = ({ name, value }: { name: string; value: string }) => {
+    const item = new ConsumedItem({
+      icon: textComprehension(name),
+      name,
+      value: formatBrlToFloat(value)
+    });
+    dispatch({ type: "addItem", item });
+  };
 
   return (
     <View style={styles.main}>
-      <Title style={styles.title}>Divide aí a conta</Title>
-      <View style={styles.newPerson}>
-        <Button onPress={reducer.onChangeView}>+ Novo candango</Button>
-        <Collapse collapsed={state.collapsedNewPerson}>
-          <Text style={styles.personAccountTitle}>
-            Conta do {state.person.name}
-          </Text>
-          <Form>
-            <View>
-              <FormItem floatingLabel>
-                <Label>Nome do fulano</Label>
-                <Input onChangeText={changeName} />
-              </FormItem>
-            </View>
-            <View style={styles.itensView}>
-              <Text>
-                O que{" "}
-                <Text style={styles.textBold}>
-                  {state.person.name || "Fulano"}
-                </Text>{" "}
-                consumiu?
-              </Text>
-              <View style={styles.itemRegister}>
-                <FormItem inlineLabel picker>
-                  <Icon name="product-hunt" type="FontAwesome" />
-                  <Text onPress={inputRef.current.focus}>Nome do item: </Text>
-                  <TextInput ref={inputRef} />
-                </FormItem>
-              </View>
-            </View>
-          </Form>
-        </Collapse>
+      <View style={styles.main}>
+        <Title style={styles.title}>Divide aí a conta</Title>
+        <View style={styles.newPerson}>
+          <Button style={styles.fluxButtons} onPress={toggleNewItem}>
+            {state.collapseItem ? "1. Novo Item" : "1. Acabou os itens"}
+          </Button>
+          <Button
+            style={styles.fluxButtons}
+            onPress={toggleNewPerson}
+            disabled={state.items.length === 0}
+          >
+            {state.collapsePerson
+              ? "2. Dividir com mais um"
+              : "2. Chega de gente"}
+          </Button>
+          <Collapse collapsed={state.collapseItem}>
+            <NewItemForm onSubmit={submit} />
+          </Collapse>
+          <Collapse collapsed={state.collapsePerson}>
+            <Text>Adicionar gente</Text>
+          </Collapse>
+        </View>
       </View>
+      <ListRegisterItems items={state.items} remove={remove} />
     </View>
   );
 };
